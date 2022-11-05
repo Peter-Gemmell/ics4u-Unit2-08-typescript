f8b06b0c1af86026357813142563371b2089a598
Peter Gemmell <peter.gemmell@mths.ca~>
2022-11-05 23:11:27 +0000

    real

--- src/Truck.ts
+++ src/Truck.ts
@@ -22,13 +22,8 @@ class Truck extends Vehicle {
 
   // status
   status(): void {
-    console.log(
-      ` → Color: ${super.getColor()}
-    → Max Speed: ${super.getMaxSpeed()}
-    → Speed: ${super.getSpeed()}
-    → LicensePlate: ${this.licensePlate}
-    → Number of tires: ${super.getTires()}`
-    )
+    super.status()
+    console.log(`    → LicensePlate: ${this.licensePlate}`)
   }
 
   // getter for licensPlate
